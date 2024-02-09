08de18b40c45a6e9ffa03b141100cff219fd121c
Joseph Benkanoun <benkanoun.greene@gmail.com>
2024-02-09 15:29:58 +0000

    updating code to define inputs as integers on the basis of code learned in Week 2 lectures

--- bank.py
+++ bank.py
@@ -1,4 +1,4 @@
-amount1 = input("Enter first amount (in cents) ")
-amount2 = input("Enter second amount (in cents) ")
+amount1 = int(input("Enter first amount (in cents): "))
+amount2 = int(input("Enter second amount (in cents): "))
 total = (amount1 + amount2)
 print (f"The sum of these is €{total}")
