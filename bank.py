5265ae70b3ab122dea73343680bfc73c4f134261
Joseph Benkanoun <benkanoun.greene@gmail.com>
2024-02-13 19:43:02 +0000

    attempting another solution to output centvalue

--- bank.py
+++ bank.py
@@ -2,12 +2,11 @@
 # author: Joseph Benkanoun
 # prompt user for two cent amounts and print output as a euro value
 
-# had an idea to define eurovalue by dividing to integer and define centvalue as remainder on the basis of arithmetic function learned in Week 2
+# still needed a solution to output a centvalue, so decided to try converting total to a string and only selecting the last two characters (the centvalue)
 amount1 = int(input("Enter first amount (in c): "))
 amount2 = int(input("Enter second amount (in c): "))
 total = (amount1 + amount2)
 eurovalue = total//100
-centvalue = total%100
-print (f"The sum of these is €{eurovalue}.{centvalue}")
+print (f"The sum of these is €{eurovalue}.{str(total)[-2:]}")
 
-# doesn't work, as if centvalue < 10, the console only prints one decimal point so 02 becomes 2 (ie 27 + 75 = €1.2)
+# works broadly but doesn't work for total values under >100 so will need to determine a solution for those
