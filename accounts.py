384e3ab96d6e8868f902d7cd2ec0d1d90912ab01
Joseph Benkanoun <benkanoun.greene@gmail.com>
2024-02-13 21:02:04 +0000

    initial attempt to play with output

---
+++ accounts.py
@@ -0,0 +1,7 @@
+# accounts.py
+# author: Joseph Benkanoun
+# read a 10 character acct number and only output the last 4 digits
+
+# quick attempt to play with the output with spare five minutes
+acc_no = str(input("Please enter a 10 digit account number: "))
+print(len((acc_no-4)))
