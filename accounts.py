0d83dbd8cb1731ff9a9370fc9b094403f2d71a20
Joseph Benkanoun <benkanoun.greene@gmail.com>
2024-02-14 14:35:37 +0000

    added a WHILE loop to generate an error message for account numbers of wrong length

--- accounts.py
+++ accounts.py
@@ -5,11 +5,13 @@
 # attempting to add an error message if account number of wrong length is input
 acc_no = str(input("Please enter a 10 digit account number: "))
 digits = acc_no[-4:]
-if len(acc_no) != 10:
+while len(acc_no) != 10:
     print("You have entered an invalid account number (incorrect number of digits)")
-    str(input("Please enter a 10 digit account number: "))
+    acc_no = str(input("Please enter a 10 digit account number: "))
 else:
     print(f"XXXXXX{digits}")
 
 # this almost works as expected except second prompt to enter account number goes nowhere
 # will have to look into ways to loop back to initial prompt rather than another prompt under IF statement
+    
+# learned about WHILE loops in Week04 which helped me solve what I was trying to do
