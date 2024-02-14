a97519b9ca0b78a5853ea6cf0a4bbeb3a531b7c6
Joseph Benkanoun <benkanoun.greene@gmail.com>
2024-02-14 11:12:22 +0000

    used IF function to account for total values < 10

--- bank.py
+++ bank.py
@@ -2,11 +2,15 @@
 # author: Joseph Benkanoun
 # prompt user for two cent amounts and print output as a euro value
 
-# still needed a solution to output a centvalue, so decided to try converting total to a string and only selecting the last two characters (the centvalue)
+# included an if function to account for anomolies when total was less than 10
+# was familiar with Excel before taking the course so had an understanding of IF functions, Googled how they applied within Python
 amount1 = int(input("Enter first amount (in c): "))
 amount2 = int(input("Enter second amount (in c): "))
 total = (amount1 + amount2)
 eurovalue = total//100
-print (f"The sum of these is €{eurovalue}.{str(total)[-2:]}")
+if total < 10:
+    print (f"The sum of these is €{eurovalue}.0{str(total)[-2:]}")
+else:
+    print (f"The sum of these is €{eurovalue}.{str(total)[-2:]}")
 
-# works broadly but doesn't work for total values under >10 so will need to determine a solution for those
+# this now seems to work as required whether value is €0.0x, €0.xx, €x.xx, or beyond
