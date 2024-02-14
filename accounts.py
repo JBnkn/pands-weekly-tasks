addda102b9fbbcf82f0265cd1c39cba595492db9
Joseph Benkanoun <benkanoun.greene@gmail.com>
2024-02-14 12:03:19 +0000

    modified code to deal with account numbers of any length

--- accounts.py
+++ accounts.py
@@ -2,10 +2,11 @@
 # author: Joseph Benkanoun
 # read a 10 character acct number and only output the last 4 digits
 
-# attempting to read last four digits as an spliced output
+# attempting to add a function to deal with account numbers of any length
 acc_no = str(input("Please enter a 10 digit account number: "))
 digits = acc_no[-4:]
-print(f"XXXXXX{digits}")
+anon = "X"*(len(acc_no)-4)
+print(f"{anon}{digits}")
 
-# this works and is straightforward enough
-# will try to code it to deal with account numbers of any length
+# I have added a line which takes overall length of account number, subtracts four from overall length, and generates that many Xs
+# it then adds those Xs as anon fields alongside the last four digits
