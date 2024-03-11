5cbed0cfc79e3efe3544de918d983083cea01f0c
Joseph Benkanoun <benkanoun.greene@gmail.com>
2024-03-11 17:16:57 +0000

    Trialled get method for dicts

--- textfile.py
+++ textfile.py
@@ -3,9 +3,6 @@
 # read in a text file and output the number of e's it contains
 # program should take the filename from an argument on the command line
 
-# initial attempt at coding the program
-# trying to focus on the count before looking at reading in the file as an argument
-
 FILENAME = "sample.txt"
 counts = {}
 
@@ -17,10 +14,11 @@ with open (FILENAME, 'r') as txt:
         else:
             counts[i] = 1
 
-lower = counts['e']
-upper = counts['E']
+lower = counts.get('e', 0)
+upper = counts.get('E', 0)
 total = lower + upper
 
 print(f"There are {total} instances of the letter E in this text. {lower} are lower case, {upper} are upper case.")
 
-# this works however throws an error if lower or upper = 0 so needs further work
+# used w3schools get method to define value for a key that doesn't exist
+# https://www.w3schools.com/python/ref_dictionary_get.asp
