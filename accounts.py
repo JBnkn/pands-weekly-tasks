ba153013feff4f3e299974eaa5fa553ee9464879
Joseph Benkanoun <benkanoun.greene@gmail.com>
2024-03-20 18:24:21 +0000

    Making an attempt to overcomment and show how I worked through the problem

--- accounts.py
+++ accounts.py
@@ -2,16 +2,19 @@
 # author: Joseph Benkanoun
 # read a 10 character acct number and only output the last 4 digits
 
-# attempting to add an error message if account number of wrong length is input
-acc_no = str(input("Please enter a 10 digit account number: "))
-digits = acc_no[-4:]
-while len(acc_no) != 10:
-    print("You have entered an invalid account number (incorrect number of digits)")
-    acc_no = str(input("Please enter a 10 digit account number: "))
-else:
-    print(f"XXXXXX{digits}")
+# prompting user to enter 10 digit number
+acc_no = input("Please enter a 10 digit account number: ")
 
-# this almost works as expected except second prompt to enter account number goes nowhere
-# will have to look into ways to loop back to initial prompt rather than another prompt under IF statement
-    
-# learned about WHILE loops in Week04 which helped me solve what I was trying to do
+# added an error message if input is in the incorrect format (eg, a string)
+while not acc_no.isdigit():
+    print("Incorrect format.")
+    acc_no = input("Please enter a 10 digit account number: ")
+else:
+
+# added an error message if account number of wrong length is input (eg, 5 digits)
+    while len(acc_no) != 10:
+        print("You have entered an invalid account number (incorrect number of digits)")
+        acc_no = str(input("Please enter a 10 digit account number: "))
+    else:
+        digits = acc_no[-4:]
+        print(f"XXXXXX{digits}")
