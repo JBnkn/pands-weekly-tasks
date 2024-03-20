47f9f02c64407d2a4121b66c9a871c3aa3200fb3
Joseph Benkanoun <benkanoun.greene@gmail.com>
2024-03-20 17:09:45 +0000

    Initial attempt on building out the hist plot

--- plottask.py
+++ plottask.py
@@ -2,7 +2,7 @@
 # author: Joseph Benkanoun
 # write a program that displays:
 # a histogram of a normal distribution of 1000 values with a mean of 5 and standard deviation of 2, and
-# a plot of the function *h(x)=x3* in the range 0 to 10
+# a plot of the function h(x)=x3 in the range 0 to 10
 # on the one set of axes.
 
 import numpy as np
