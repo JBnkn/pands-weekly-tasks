1e418bb9622cd5d1b930ddbf09b3fd26abc95ffb
Joseph Benkanoun <benkanoun.greene@gmail.com>
2024-03-21 14:07:29 +0000

    reviewing code and overcommenting

--- textfile.py
+++ textfile.py
@@ -3,21 +3,32 @@
 # read in a text file and output the number of e's it contains
 # program should take the filename from an argument on the command line
 
-FILENAME = "sample.txt"
+# prompt user to enter a filename (I have created sample.txt in the folder for demo purposes)
+FILENAME = str(input("Please enter a filename (txt format only): "))
+
+# set an empty dict to capture the counts
 counts = {}
 
+# open the file
 with open (FILENAME, 'r') as txt:
     data = txt.read()
+    # run through the data to capture instances of each character
     for i in data:
+    # if the character is already in the dict, add one to the count
         if i in counts:
             counts[i] += 1
+    # if not, assign it one for the first count
         else:
             counts[i] = 1
 
+# check the counts for e and E in the dict
 lower = counts.get('e', 0)
 upper = counts.get('E', 0)
+
+# combine them for the total
 total = lower + upper
 
+# print the total value and counts for lower and upper
 print(f"There are {total} instances of the letter E in this text. {lower} are lower case, {upper} are upper case.")
 
 # used w3schools get method to define value for a key that doesn't exist
